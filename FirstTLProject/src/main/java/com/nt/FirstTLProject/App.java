85bdefbfb59854b68875c3603c881857e25dc690
prathima <kotha.prathima963@gmail.com>
2024-07-18 17:03:09 +0530

    first commit

---
+++ FirstTLProject/src/main/java/com/nt/FirstTLProject/App.java
@@ -0,0 +1,14 @@
+package com.nt.FirstTLProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.print("Hello TL");
+    }
+}
