7710efcdcac372b692878ddf1c7984e15f4ffc12
prathima <kotha.prathima963@gmail.com>
2024-07-18 17:50:47 +0530

    TL line 14 commit

--- FirstTLProject/src/main/java/com/nt/FirstTLProject/App.java
+++ FirstTLProject/src/main/java/com/nt/FirstTLProject/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.print("Hello TL");
         System.out.println();
+        System.out.println("TL added sopln");
     }
 }
